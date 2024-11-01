971351bf4f3fae59c6c9366ebbb71696f638ba17
mahmoudAliaboElhassan <ml6893254@gmail.com>
2024-11-01 21:32:16 +0200

    loader

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -1,11 +1,13 @@
+import { useTranslation } from "react-i18next";
 import "./loader.css";
 
 function Loader() {
+  const { t } = useTranslation();
   return (
     <div className="loader-container">
       <div className="page-loader">
-        <div data-glitch="Loading..." className="glitch">
-          Loading...
+        <div data-glitch={t("loading")} className="glitch">
+          {t("loading")}
         </div>
       </div>
     </div>
